8b0c8937d697452bfedbb78bd19d5c22b0eca6b7
kyounghoon <kkh2826@naver.com>
2021-05-16 09:30:29 +0900

    django-crispy-forms 적용하기

--- HoonLog/settings.py
+++ HoonLog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'crispy_forms',
     'blog',
     'single_pages',
 ]
@@ -125,4 +126,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
