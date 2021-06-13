fda02ac2381466b0be2e96a5193c0b067e3ba0b8
kyounghoon <kkh2826@naver.com>
2021-06-13 09:38:07 +0900

    댓글 삭제 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # FBV 형태
-    #path('', views.index),
-    #path('<int:pk>/', views.single_post_page),
 
     # CBV 형태
     path('', views.PostList.as_view()),
@@ -13,6 +10,7 @@ urlpatterns = [
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
 ]
